75f02015daf7e06e7a44ce87cb7bbf3a549d212b
Lior Tamir <ltamir@localhost.localdomain>
2020-09-29 12:35:22 +0300

    links and forms checked per link

--- scanner.py
+++ scanner.py
@@ -26,7 +26,6 @@ class Scanner:
                 link = link.split("#")[0]
             if self.target_url in link and link not in self.target_links and link not in self.links_to_ignore:
                 self.target_links.append(link)
-                print(link)
                 self.crawl(link)
     
     def extract_forms(self, url):
@@ -55,6 +54,23 @@ class Scanner:
         for link in self.target_links:
             forms = self.extract_forms(link)
             for form in forms:
-                print("[+] Testing form in " + link)
+                print("Testing " + link)
+                is_vuln_to_xss = self.test_xss_in_form(form, link)
+                if is_vuln_to_xss:
+                    print("\n\n[+] XSS found in " + link + " in form " + str(form))
             if "=" in link:
-                print("[+] Testing " + link)
+                print("Testing " + link)
+                is_vuln_to_xss = self.test_xss_in_link(link)
+                if is_vuln_to_xss:
+                    print("\n\n[+] XSS found in " + link)
+    
+    def test_xss_in_link(self, url):
+        xss_test_script = "<sCript>alert('test_xss_in_link')</scriPt>"
+        url = url.replace("=", "=" + xss_test_script)
+        response = self.session.get(url)
+        return xss_test_script in response.content
+
+    def test_xss_in_form(self, form, url):
+        xss_test_script = "<sCript>alert('xss')</scriPt>"
+        response = self.submit_form(form, xss_test_script, url)
+        return xss_test_script in response.content
